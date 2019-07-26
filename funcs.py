cf84924b5b6fcd9fa613b2a37aa161beb68febbc
armaneshaghi <arman.eshaghi@gmail.com>
2019-07-26 16:55:22 +0100

    BUG: added parentheses for prepare_data

--- funcs.py
+++ funcs.py
@@ -31,8 +31,8 @@ def prepare_data(pat_data, hc_data):
 
     '''
     z_score_array = np.zeros( pat_data.shape )
-    if (np.nunsum( pat_data ) > 0 ) or (np.nunsum(hc_data) > 0 ):
-        raise Exception("Patient or healthy control data has NaNs. Please remove or impute." )
+    if ((np.nunsum( pat_data ) > 0 ) or (np.nunsum(hc_data) > 0 )):
+        raise Exception("Patient or healthy control data have NaNs. Please remove or impute." )
     #calculate z-score
     for column_number, (sample_mean, sample_std) in enumerate( zip( np.mean( hc_data, axis = 0),
                                                                     np.std( hc_data, axis = 0 ))):
