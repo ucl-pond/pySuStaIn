BUG: covariance matrix missing

funcs.py
@@ -291,7 +291,7 @@ def estimate_ml_sustain_model_nplus1_clusters(data,
     else:
         # If the number of subtypes is greater than 1, go through each subtype
         # in turn and try splitting into two subtypes
-        _, _, _, p_sequence, _ = calculate_likelihood_mixturelinearzscoremodels(data,
+        _, _, _, p_sequence, _ = calculate_likelihood_mixturelinearzscoremodels( data,
                                                                                 min_biomarker_zscore,
                                                                                 max_biomarker_zscore,
                                                                                 std_biomarker_zscore,
@@ -300,7 +300,7 @@ def estimate_ml_sustain_model_nplus1_clusters(data,
                                                                                 ml_sequence_prev,
                                                                                 ml_f_prev,
                                                                                 likelihood_flag,
-                                                                                covar)
+                                                                                covar )
         ml_sequence_prev = ml_sequence_prev.reshape(ml_sequence_prev.shape[0], ml_sequence_prev.shape[1])
         p_sequence = p_sequence.reshape(p_sequence.shape[0], N_S - 1)
         p_sequence_norm = p_sequence / np.tile(np.sum(p_sequence, 1).reshape(len(p_sequence), 1), (N_S - 1))
@@ -348,7 +348,8 @@ def estimate_ml_sustain_model_nplus1_clusters(data,
                                                                                                                                                                  this_f_init,
                                                                                                                                                                  N_startpoints,
                                                                                                                                                                  likelihood_flag,
-                                                                                                                                                                 num_cores)
+                                                                                                                                                                 num_cores,
+                                                                                                                                                                 covar)
                 # Choose the most probable SuStaIn model from the different
                 # possible SuStaIn models initialised by splitting each subtype
                 # in turn
@@ -809,9 +810,9 @@ def calculate_likelihood_stage_linearzscoremodel_approx(data,
     p_perm_k = np.zeros((M, N + 1))
 
     # optimised likelihood calc - take log and only call np.exp once after loop
-    if isinstance(covar,np.ndarray):#if covariance matrix is set (not False) 
-        # FIXME: this should be calculated from control data
-        sigmat = np.tile( covar, (M, 1, 1))
+    if isinstance(covar,np.ndarray):#if covariance matrix is set
+        #sigmat = np.tile( covar, (M, 1, 1))
+        sigmat = covar
     else:
         sigmat = np.tile(std_biomarker_zscore, (M, 1))    
         factor = np.log(1. / np.sqrt(np.pi * 2.0) * sigmat)
@@ -824,7 +825,8 @@ def calculate_likelihood_stage_linearzscoremodel_approx(data,
         if isinstance(covar,np.ndarray): #if covariance matrix is provided
             temp = []
             for i in range(stage_value_tiled_j.shape[0]):
-                temp.append(1./(N+1.)*multivariate_normal.pdf(data[i,:],stage_value_tiled_j[i,:],sigmat[i]))
+                #temp.append(1./(N+1.)*multivariate_normal.pdf(data[i,:],stage_value_tiled_j[i,:],sigmat[i]))
+                temp.append(1./(N+1.)*multivariate_normal.pdf(data[i,:],stage_value_tiled_j[i,:],sigmat))
             p_perm_k[:, j] = temp
         else:
             x = (data - stage_value_tiled_j) / sigmat

simrun.py
@@ -73,8 +73,8 @@ def main():
     shape_cov = np.cov(np.transpose( data ) ).shape
     covar = np.random.rand( shape_cov[0], shape_cov[1] )
     #make sure it is positive semi-definite
-    covar = np.linalg.cholesky( np.dot(covar,covar.transpose()))
-    covar=False
+    covar = np.dot(covar,covar.transpose())
+    #covar=False
     samples_sequence, samples_f = run_sustain_algorithm(data,
                                                         min_biomarker_zscore,
                                                         max_biomarker_zscore,
@@ -103,7 +103,7 @@ def main():
         
         if not test_idxs:
             print(
-                '!!!CAUTION!!! No user input for cross-validation fold selection - using automated stratification. Only do this if you know what you are doing!')
+                '!!!CAUTION!!! No user input for cross-validation fold selection - using automated stratification.')
             N_folds = 10
             if target:
                 cv = StratifiedKFold(n_splits=N_folds, shuffle=True)
@@ -146,4 +146,4 @@ def main():
 
 if __name__ == '__main__':
     np.random.seed(42)
-    kkkkkkmain()
+    main()