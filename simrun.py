57530622a383def4aa56579a3e329a0b207e5651
Arman Eshaghi <armaneshaghi@users.noreply.github.com>
2019-09-19 14:04:04 +0100

    Update simrun.py

--- simrun.py
+++ simrun.py
@@ -89,7 +89,7 @@ def main():
                                                         dataset_name,
                                                         N_iterations_MCMC_opt, 
                                                         num_cores,
-                                                        covar )
+                                                        covar = False )
 
     if validate:
         ### USER DEFINED INPUT: START
