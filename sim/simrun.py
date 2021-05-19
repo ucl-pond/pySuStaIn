21b0a372b1b41707848ce2c4e36a855ac392dc72
noxtoby <n.oxtoby@ucl.ac.uk>
2021-05-19 11:26:27 +0100

    fixed import

--- sim/simrun.py
+++ sim/simrun.py
@@ -14,7 +14,7 @@ import pandas as pd
 from simfuncs import *
 
 from kde_ebm.mixture_model import fit_all_gmm_models, fit_all_kde_models
-import kde_ebm.plotting as plotting
+from kde_ebm import plotting
 
 import warnings
 warnings.filterwarnings("ignore",category=cbook.mplDeprecation)
