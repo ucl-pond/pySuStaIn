69d4f70d6de87f511a824dd2c24ff18874b99b73
noxtoby <n.oxtoby@ucl.ac.uk>
2021-05-19 09:51:28 +0100

    Minor fixes

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # Authors: Leon Aksman <l.aksman@ucl.ac.uk>
 # License: TBC
-
+__version__ = '0.1'
 
 from setuptools import setup
 
@@ -15,21 +15,21 @@ with open('requirements.txt', 'r') as f:
 
 print("Started pySuStaIn setup.py")
 
-setup(name=					'pySuStaIn',
-      version=				'0.1',
-      description=			'Python implementation of the SuStaIn algorithm',
-      url=					'https://github.com/ucl-pond/pySuStaIn',
-      classifiers=			['Intended Audience :: Science/Research',
-                   			'Programming Language :: Python',
-                   			'Topic :: Scientific/Engineering',
-                   			'Programming Language :: Python :: 3.7'],
-      maintainer=			'Leon Aksman',
-      maintainer_email=		'l.aksman@ucl.ac.uk',
-      license=				'TBC',
-      packages=				['pySuStaIn', 'sim'],
-      python_requires= 		'>=3.7',
-      install_requires = 	install_reqs,	#the parsed requirements from requirements.txt
-      entry_points=			{},
-      zip_safe=				False)
+setup(name=               'pySuStaIn',
+      version=            __version__,
+      description=        'Python implementation of the SuStaIn algorithm',
+      url=                'https://github.com/ucl-pond/pySuStaIn',
+      classifiers=			  ['Intended Audience :: Science/Research',
+                   			  'Programming Language :: Python',
+                   			  'Topic :: Scientific/Engineering',
+                   			  'Programming Language :: Python :: 3.7'],
+      maintainer=         'Leon Aksman',
+      maintainer_email=   'l.aksman@ucl.ac.uk',
+      license=		        'TBC',
+      packages=			      ['pySuStaIn', 'sim'],
+      python_requires=  	'>=3.7',
+      install_requires =  install_reqs,	#the parsed requirements from requirements.txt
+      entry_points=			  {},
+      zip_safe=				    False)
 
 print("Finished pySuStaIn setup.py")
